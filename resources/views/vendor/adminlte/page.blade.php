af40274491dcfe9114497ad3b010eab05287f8a8
Tobias Lang <tlang@fintecsystems.com>
2019-03-23 22:20:15 +0100

    Display user avatar in sidebar.

--- resources/views/vendor/adminlte/page.blade.php
+++ resources/views/vendor/adminlte/page.blade.php
@@ -90,7 +90,14 @@
 
             <!-- sidebar: style can be found in sidebar.less -->
             <section class="sidebar">
-
+                <div class="user-panel">
+                    <div class="pull-left image">
+                        <img src="{{ Auth::user()->avatar }}" class="img-circle" alt="User Image">
+                    </div>
+                    <div class="pull-left info">
+                        <p>{{ Auth::user()->name }}</p>
+                    </div>
+                </div>
                 <!-- Sidebar Menu -->
                 <ul class="sidebar-menu" data-widget="tree">
                     @each('adminlte::partials.menu-item', $adminlte->menu(), 'item')
